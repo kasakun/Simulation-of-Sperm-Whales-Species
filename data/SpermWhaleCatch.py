f3b969f320fb97d33eaa71e54c161b596a564bcb
Zeyu Chen <chenzy@gatech.edu>
2018-03-31 00:16:31 -0400

    Updated

--- data/SpermWhaleCatch.py
+++ data/SpermWhaleCatch.py
@@ -1,3 +1,6 @@
+# Created on 03/30/2018
+# Taoyouwei Gao
+
 import urllib.request
 import time
 import csv
@@ -6,7 +9,7 @@ from bs4 import BeautifulSoup
 
 def getYearUrl(year):
 
-	url = 'http://luna.pos.to/whale/sta_'+year+'.html' 
+	url = 'http://luna.pos.to/whale/sta_'+year+'.html'
 	soup=BeautifulSoup(urllib.request.urlopen(url),"lxml")
 	#temp=urllib.request.urlopen(req).read()
 	trs = soup.find_all('tr')
@@ -21,7 +24,7 @@ def getYearUrl(year):
 		if uilist[x][1]=='TOTAL':
 			index=x
 			break
-	print('正在下载'+year+'.csv')
+	print('Downloading '+year+'.csv')
 	with open("/Users/matthewgao/Downloads/python/spermWhaleCatch.csv","a+") as csvfile:
 	    writer = csv.writer(csvfile)
 	    list=[]
@@ -33,4 +36,3 @@ if __name__ == '__main__':
 
 	for i in range(1910,2014):
 		getYearUrl(str(i))
-
